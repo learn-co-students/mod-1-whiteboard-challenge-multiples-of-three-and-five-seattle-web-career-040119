71e659b4a92094f54ad391fe3e84a716d0799ab2
vibraniumforge <gregm9999@gmail.com>
2019-09-12 09:16:43 -0700

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,19 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+  final_ar = []
+  1.upto(limit -1 ) do |x|
+		if x % 3 == 0 || x % 5 == 0
+			final_ar << x
+		end
+	end
+	final_ar
+end
+
+def sum_multiples(limit)
+	final_ar = []
+  1.upto(limit -1 ) do |x|
+		if x % 3 == 0 || x % 5 == 0
+			final_ar << x
+		end
+	end
+	final_ar.sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,29 @@
-# Enter your object-oriented solution here!
+class Multiples
+
+  attr_reader :limit
+
+	def initialize(limit)
+		@limit = limit
+	end
+
+	def collect_multiples
+		final_ar = []
+		1.upto(limit-1) do |x|
+			if x % 3 == 0 || x % 5 == 0
+				final_ar << x
+			end
+		end
+		final_ar
+	end	
+
+	def sum_multiples
+		total = 0
+		1.upto(limit-1) do |x|
+			if x % 3 == 0 || x % 5 == 0
+				total += x
+			end
+		end
+		total
+	end	
+
+end
